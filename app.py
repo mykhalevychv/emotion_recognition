8ba44a3ddb15f7b43d979c20a465bbcb607a74fa
Victoria Mykhalevych <37329041+mykhalevychv@users.noreply.github.com>
2021-06-09 16:27:11 +0300

    -

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def gen():
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
+    """Video streaming route"""
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
